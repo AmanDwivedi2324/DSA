0558a4988f3b592ec148846f9bbc131ae7721330
AmanDwivedi2324 <0786ad2000@gmail.com>
2024-10-05 23:53:15 +0530

    Solve pattern questions

---
+++ JavaBasics.java
@@ -0,0 +1,104 @@
+
+public class JavaBasics {
+    public static void main(String[] args) {
+
+        /*  * * * * 
+            * * * 
+            * * 
+            * 
+        */ 
+
+        // for(int i=4;i>=1;i--){
+        //     for(int j=i;j>=1;j--){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        // }
+
+
+        /*
+         *
+         **
+         ***
+         ****
+
+         
+         */
+
+        //  for(int i=1;i<=4;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print("* ");
+        //     }
+        //     System.out.println();
+        //  }
+
+        /*
+         *****
+         *   *
+         *   * 
+         *****
+         */
+       
+        // for(int i=1;i<=4;i++){
+        //    for(int j = 1;j<=4;j++){
+        //     if(i == 1 || i == 4 || j == 1 || j == 4){
+        //         System.out.print("*");
+        //     }
+        //     else{
+        //         System.out.print(" ");
+        //     }
+        //    }
+        //    System.out.println();
+        // }
+
+        /*
+           *
+          **
+         ***   
+        ****
+        
+        */
+
+        // for(int i=1;i<=4;i++){
+        //     for(int j=1;j<=4-i;j++){
+        //         System.out.print(" ");
+        //     }
+        //     for(int k=1;k<=i;k++){
+        //         System.out.print("*");
+        //     }
+        //     System.out.println();
+        // }
+
+        /*
+         12345
+         1234
+         123
+         12
+         1
+         */
+
+        //  for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=5-i+1;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        //  }
+
+        /*
+        1
+        12
+        123
+        1234
+        12345 
+
+         */
+
+        //  for(int i=1;i<=5;i++){
+        //     for(int j=1;j<=i;j++){
+        //         System.out.print(j);
+        //     }
+        //     System.out.println();
+        //  }
+
+    }
+}
